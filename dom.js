977c12d41dba0a2b2c8b76bb6ad0d2132f7a20ee
Samuel Kay <samuelkay505@gmail.com>
2026-09-16 15:42:25 +0100

    Update dom.js

--- dom.js
+++ dom.js
@@ -331,6 +331,9 @@ jUtils.fn.at = function (index = 0) {
 // Collect the current matched elements as a mapped array.
 const elements = this.get(el => el, 'map');
 
+// return if no element was found.
+if(!elements) return this;
+ 
 // Convert negative indexes into a valid positive position.
 const len = $.pos(elements.length).loose(index);
 
