a0bad183f4454411fa941812ecd3d27c820ebbb4
Samuel Kay <samuelkay505@gmail.com>
2026-09-17 03:36:16 +0100

    Fix array handling for input elements

--- core.js
+++ core.js
@@ -64,8 +64,8 @@ if(typeof input === 'string') {
  [].concat(input).forEach(item => {     
   if(item instanceof jUtils) {
    result.push(...item.elements);
-  } else if(item && Array.from(item).some(el => el && el.nodeType === 1)) {
-   result.push(Array.from(item));
+  } else if(item && Array.from(item).every(el => el && el.nodeType === 1)) {
+   result.push(...Array.from(item));
   } else { 
    result.push(item);
   }
