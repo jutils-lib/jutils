3b5acf00cbb3c71060f153650f06c05e8deecdf4
Samuel Kay <samuelkay505@gmail.com>
2026-09-17 09:51:12 +0100

    Update util.js

--- util.js
+++ util.js
@@ -2112,7 +2112,6 @@ const req = store.openCursor();
  const cursor = event.target.result;          
   if(cursor) {
    const current = cursor.value;   
-   delete current[primaryKey];
    
    const result = [current].find(normalizeQuery(query));    
    if(result) callback({ result, cursor });
@@ -2210,6 +2209,7 @@ config(query, ({ missing, result }) => {
 if(missing) {
 resolve(undefined);
 } else {
+delete result[primaryKey];
 resolve(field === undefined ? result : result[field]);  
 }
 }); 
@@ -2229,6 +2229,7 @@ return item;
 });
 resolve(items.slice(0, limit));
 } else {
+delete result[primaryKey];
 items.push(result);
 }
 });
