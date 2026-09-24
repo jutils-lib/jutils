5a9d03bbf23c56f24e16d7ede979e19f0a4736a8
Samuel Kay <samuelkay505@gmail.com>
2026-09-24 04:36:18 +0100

    Update util.js

--- util.js
+++ util.js
@@ -18,10 +18,10 @@
 $.compute = function (input, ...args) {
   // If the input is a function, execute it with the remaining arguments.
   // Otherwise, return the input unchanged.
-  return typeof input === 'function' ? input(...args) : input;   
+  return typeof input === 'function' ? input(...args) : input; 
+ // Normally 
 }
 
-  $.compute = 86;
 
 
 /**
